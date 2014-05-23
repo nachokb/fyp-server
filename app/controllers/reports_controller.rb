b3cb4332482a5a296abf282349ac28d0fdcd9d1c
Ignacio Carrera <ignacio.carrera@inakanetworks.com>
2014-05-23 13:22:34 -0300

    ReportsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,10 +1,19 @@
 class ReportsController < ApplicationController
   def index
-    Report.search_all(safe_params)
+    # sort_params should be nil if there's no geo
+    # filter_params should be {} if there's no criteria
+    Report.search_all(sort_params, {})
+  end
+
+  def create
+
   end
 
   private
-  def safe_params
+  def sort_params
     params.permit(:lat, :lon)
+  end
+  def filter_params
+    params.permit(:species, :race)
   end
 end
