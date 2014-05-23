f9a2007baaba469e3e406f766d604156c8fcdf12
Ignacio Carrera <ignacio.carrera@inakanetworks.com>
2014-05-23 13:00:09 -0300

    S3 fixes, gitignore

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,2 +1,10 @@
 class SightingsController < ApplicationController
+  def index
+    Sighting.search_all(safe_params)
+  end
+
+  private
+  def safe_params
+    params.permit(:lat, :lon)
+  end
 end
--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,6 +1,6 @@
 class Sighting < ActiveRecord::Base
   has_attached_file :picture, 
     s3_credentials: Rails.root + '/config/s3.yml',
-    s3_bucket: 'inakathoon-kiss',
+    bucket: 'inakathoon-kiss',
     default_url: "/images/missing.png"
 end
